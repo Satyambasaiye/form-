5cbca848360c4919c2aad6c04bbae4aaaf345a62
Satyam Basaiye <satyambasaiye123@gmail.com>
2023-04-07 14:06:11 +0530

    added new fields

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Form from './components/Form';
 function App() {
   return (
     <div>
+      <h1 style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>Trek form </h1>
       <Form />
     </div>
     
--- src/components/Form.js
+++ src/components/Form.js
@@ -2,15 +2,23 @@ import React, { useState } from 'react';
 import axios from 'axios';
 import './styles.css';
 
-
 const Form = () => {
   const [title, setTitle] = useState('');
   const [description, setDescription] = useState('');
   const [locations, setLocations] = useState('');
-  const [date, setDate] = useState('');
+  const [fromDate, setFromDate] = useState('');
+  const [toDate, setToDate] = useState('');
   const [actualPrice, setActualPrice] = useState('');
   const [discountedPrice, setDiscountedPrice] = useState('');
   const [image, setImage] = useState(null);
+  const [itinerary, setItinerary] = useState('');
+  const [thingsToCarry, setThingsToCarry] = useState('');
+  const [pickUpPoint, setPickUpPoint] = useState('');
+  const [includes, setIncludes] = useState('');
+  const [excludes, setExcludes] = useState('');
+  const [contactDetails, setContactDetails] = useState('');
+  const [cancellationPolicy, setCancellationPolicy] = useState('');
+
 
   const handleSubmit = async (event) => {
     event.preventDefault();
@@ -22,9 +30,17 @@ const Form = () => {
     formData.append('title', title);
     formData.append('description', description);
     formData.append('locations', locations);
-    formData.append('date', date);
+    formData.append('fromDate', fromDate);
+    formData.append('toDate', toDate);
     formData.append('actualPrice', actualPrice);
     formData.append('discountedPrice', discountedPrice);
+    formData.append('itinerary', itinerary);
+    formData.append('thingsToCarry', thingsToCarry);
+    formData.append('pickUpPoint', pickUpPoint);
+    formData.append('includes', includes);
+    formData.append('excludes', excludes);
+    formData.append('contactDetails', contactDetails);
+    formData.append('cancellationPolicy', cancellationPolicy);
 
     // Append the selected file(s) to the FormData object
     if (image !== null) {
@@ -56,17 +72,50 @@ const Form = () => {
         <input type="text" value={locations} onChange={(e) => setLocations(e.target.value)} />
       </label>
       <label>
-        Date:
-        <input type="text" value={date} onChange={(e) => setDate(e.target.value)} />
+        From Date:
+        <input type="date" value={fromDate} onChange={(e) => setFromDate(e.target.value)} />
+      </label>
+      <label>
+        To Date:
+        <input type="date" value={toDate} onChange={(e) => setToDate(e.target.value)} />
       </label>
       <label>
         Actual Price:
-        <input type="text" value={actualPrice} onChange={(e) => setActualPrice(e.target.value)} />
+        <input type="number" value={actualPrice} onChange={(e) => setActualPrice(e.target.value)} />
       </label>
       <label>
         Discounted Price:
-        <input type="text" value={discountedPrice} onChange={(e) => setDiscountedPrice(e.target.value)} />
+        <input type="number" value={discountedPrice} onChange={(e) => setDiscountedPrice(e.target.value)} />
       </label>
+      <label>
+      Itinerary:
+      <textarea value={itinerary} onChange={(e) => setItinerary(e.target.value)} />
+    </label>
+      <label>
+      Things to Carry:
+      <textarea value={thingsToCarry} onChange={(e) => setThingsToCarry(e.target.value)} />
+    </label>
+    <label>
+      Pick Up Point:
+      <input type="text" value={pickUpPoint} onChange={(e) => setPickUpPoint(e.target.value)} />
+    </label>
+    <label>
+      Includes:
+      <textarea value={includes} onChange={(e) => setIncludes(e.target.value)} />
+    </label>
+    <label>
+      Excludes:
+      <textarea value={excludes} onChange={(e) => setExcludes(e.target.value)} />
+    </label>
+    <label>
+      Contact Details:
+      <input type="text" value={contactDetails} onChange={(e) => setContactDetails(e.target.value)} />
+    </label>
+    <label>
+      Cancellation Policy:
+      <textarea value={cancellationPolicy} onChange={(e) => setCancellationPolicy(e.target.value)} />
+    </label>
+    <label></label>
       <label>
         Image:
         <input type="file" onChange={(e) => setImage(e.target.files[0])} />
